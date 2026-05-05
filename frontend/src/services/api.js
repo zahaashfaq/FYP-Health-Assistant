822b0bac915ccb6022dd0a6a0950651a2c19541d
Zaha  Ashfaq <95704048+zahaashfaq@users.noreply.github.com>
2026-05-05 21:20:07 +0500

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 //const API_URL = process.env.REACT_APP_API_URL || "/api";
 // If you want to bypass the proxy in development, set REACT_APP_API_URL in .env.
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = "https://fyp-health-assistant.onrender.com";
 console.log("API_URL:", import.meta.env.VITE_API_URL);
 export const api = {
     // ── Auth ────────────────────────────────────────────────────────────────────
